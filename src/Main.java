1bb7d7d91753bce097e6ba93510081df90428bbf
Shakib0000 <BT_4N2_05@BKTECH.LOCAL>
2023-09-19 13:53:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        System.out.print("Enter the total bill: ");
+        
+    }
+}
